f19f87f7d527b7ce979da3432bc929059eb60e90
lowrender <lowrenderxw@gmail.com>
2025-09-29 18:15:16 +0600

    added exceptions

---
+++ src/main/java/com/mafia/mafiax/exceptions/RoomNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mafia.mafiax.exceptions;
+
+public class RoomNotFoundException extends RuntimeException {
+    public RoomNotFoundException(String message) {
+        super(message);
+    }
+}
